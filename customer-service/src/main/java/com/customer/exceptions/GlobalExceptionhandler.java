2d596144b22be00dbdd71a3543ce44d343e06d4a
daton <datonarinze@gmail.com>
2022-09-05 10:54:17 +0100

    Added PDF

---
+++ customer-service/src/main/java/com/customer/exceptions/GlobalExceptionhandler.java
@@ -0,0 +1,18 @@
+package com.customer.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionhandler extends ResponseEntityExceptionHandler {
+
+        @ExceptionHandler(CustomException.class)
+        public ResponseEntity<ErrorResponse> handlerForFailedMailException(final CustomException ex) {
+            ErrorResponse errorResponse = new ErrorResponse();
+            errorResponse.setMessage(ex.getMessage());
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+
+}}
